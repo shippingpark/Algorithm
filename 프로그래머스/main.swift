c153168efc30e7fab0cf0c2a2b53a63d4a568364
hyeunPark <shippingbok@gmail.com>
2022-12-16 21:54:13 +0900

    프로그래머스 - 숫자 짝꿍

--- 프로그래머스/main.swift
+++ 프로그래머스/main.swift
@@ -1,11 +1,47 @@
-//
-//  main.swift
-//  Algorithm
-//
-//  Created by 박혜운 on 2022/12/16.
-//
+
+
+//프로그래머스 숫자 짝꿍
 
 import Foundation
 
-print("Hello, World!")
+func solution(_ X:String, _ Y:String) -> String {
+    var standardNumber: [Character:Int] = [:]
+    var resultValue = [Int](repeating: 0, count: 10)
+    var result:String = ""
+    
+    for x in X {
+        guard standardNumber[x] != nil else {standardNumber[x] = 1; continue}
+        standardNumber[x]! += 1
+    }
+    
+    for y in Y {
+        guard standardNumber[y] != nil else {continue}
+        guard standardNumber[y] != 0 else {continue}
+        resultValue[Int(String(y))!] += 1
+        standardNumber[y]! -= 1
+    }
+    
+    if resultValue.reduce(0, {$0 + $1}) == 0 {
+        return "-1"
+    }
+    
+    for myIndex in 0...9 {
+        guard resultValue[9-myIndex] != 0 else {continue}
+        var count = resultValue[9-myIndex]
+        for _ in 1...count {
+            result += "\(9-myIndex)"
+        }
+    }
+    
+    if result.first == "0" {
+        return "0"
+    }
+    
+    return result
+}
 
+print(solution("11","0")) //-1
+print(solution("100","203045")) //0
+print(solution("5525", "1255")) //552
+print(solution("0000", "11111")) //-1
+print(solution("111231", "112223")) //3211
