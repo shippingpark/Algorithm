4b81e09c95c1efe145930ec515457529ebde827b
hyeunPark <shippingbok@gmail.com>
2022-12-18 17:10:27 +0900

    LV2.점 찍기 (시간초과)
    시간 초과 3 문제

---
+++ 프로그래머스/Lv2.디펜스 게임.swift
@@ -0,0 +1,94 @@
+
+
+//프로그래머스 디펜스 게임
+
+
+import Foundation
+
+//n 병사 수
+//k 디펜스 수
+//enemy 라운드 별 적군 수
+
+func solution(_ n:Int, _ k:Int, _ enemy:[Int]) -> Int {
+    var clearRound:Int = 0
+    var defense:Int = k
+    var user:Int = n
+    var cropEnemy:[Int] = enemy
+    
+    while user > 0 {
+        //1) 남은 라운드 총 합이 유저 수 보다 적으면 끝
+        if checkClear(user, cropEnemy) {
+            clearRound += cropEnemy.count
+            return clearRound
+        }
+        
+        //2) 방어권이 없다면 (-) 되기 전에 종료
+        if defense == 0 {
+            for eachRound in cropEnemy {
+                guard user != 0 else {return clearRound}
+                guard user > 0 else {return clearRound - 1}
+                user -= eachRound
+                clearRound += 1
+            }
+            return clearRound
+        }
+        
+            //3) 방어권이 있다면
+            //3-1) cropEnemy의 합이 음수가 되는 index 불러오기
+            let minusSumIndex = searchArangeIdex(user, cropEnemy)
+            let plusSumIndex = minusSumIndex - 1
+            //3-2) 사용 후 user 값 계산하기
+            var testEnemy = cropEnemy[0...minusSumIndex]
+//            print("프린트 마이너스 인덱스 \(minusSumIndex)")
+            let maxRound = testEnemy.max()! //삭제할 최대 값
+            user = user - testEnemy.reduce(0,{$0+$1}) + maxRound
+//            print("\(user) = user - \(testEnemy.reduce(0,{$0+$1})) + \(maxRound)")
+            //3-3) 지난 라운드 만큼 배열 줄이기
+            cropEnemy.removeSubrange(0...minusSumIndex)
+            clearRound += minusSumIndex + 1
+            defense -= 1
+            
+    }
+    
+    return clearRound
+}
+ 
+
+// MARK: - 디펜스 안해도 되는 지
+func checkClear (_ n:Int, _ enemy:[Int]) -> Bool {
+    if n >= enemy.reduce(0, {$0+$1}) {
+        return true
+    }
+    return false
+}
+
+ 
+// MARK: - 디펜스 해야할 때
+//우리 병사 수가 양수가 아니게 되는 최대 index
+func searchArangeIdex (_ n:Int, _ enemy:[Int]) -> Int {
+    var maximumIdex = -1
+    var totalEnemyCount = 0
+    for eachEnemy in enemy {
+        guard totalEnemyCount < n else {return maximumIdex}
+        totalEnemyCount += eachEnemy
+//        print("enemy, totalEnemyCount:\(enemy),\(totalEnemyCount)")
+        maximumIdex += 1
+    }
+    return maximumIdex
+}
+
+
+print(solution(2,4,[3,3,3,3]))//4
+print("다음문제🔥")
+print(solution(7,3,[4,2,4,5,3,3,1]))//5
+print("다음문제🔥")
+print(solution(1,3,[1,1,1,1,3,3,1]))//4
+print("다음문제🔥")
+print(solution(4,1,[4,4,4,5,3,3,1]))//2
+print("다음문제🔥")
+print(solution(5,2,[1,2,1,3,3,3,1]))//5
+print("다음문제🔥")
+print(solution(5,2,[1,2,1,3,3,3,1]))//5
+print("다음문제🔥")
+print(solution(4,1,[4,2,2]))//3
+
--- 프로그래머스/main.swift
+++ 프로그래머스/main.swift
@@ -1,94 +1,71 @@
 
 
-//프로그래머스 디펜스 게임
-
+//점 찍기
 
 import Foundation
 
-//n 병사 수
-//k 디펜스 수
-//enemy 라운드 별 적군 수
 
-func solution(_ n:Int, _ k:Int, _ enemy:[Int]) -> Int {
-    var clearRound:Int = 0
-    var defense:Int = k
-    var user:Int = n
-    var cropEnemy:[Int] = enemy
+//시간 에러
+//func solution(_ k:Int, _ d:Int) -> Int64 {
+//    var point:Int64 = 0
+//
+//    for i in 0...d {
+//        var x = k*i
+//        for j in 0...d {
+//            var y = k*j
+//            if x*x + y*y <= d*d {
+//                point += 1
+//            }
+//        }
+//    }
+//    return point
+//}
+
+
+//중첩 for 문 피하기
+
+func solution(_ k:Int, _ d:Int) -> Int64 {
+    var result:Int64 = 0
+    var point:[Int] = []
+    //가능한 k배수 좌표 모음 (x y 관계 없음)
+    for i in 0...d {
+        var x = k*i
+        guard x <= d else {break}
+        point.append(x)
+    } //최소 0으로 확정, 최댓값 구하는 과정
+    let fullLineCount:Int = point.count
     
-    while user > 0 {
-        //1) 남은 라운드 총 합이 유저 수 보다 적으면 끝
-        if checkClear(user, cropEnemy) {
-            clearRound += cropEnemy.count
-            return clearRound
+    //줄 당 몇 개 인지 찾기
+    for eachPoint in point {
+        var x = point.last!
+        var y = eachPoint
+        var outPoint:Int = 0
+        //피타고라스 밖의 점이면
+        while !checkInnerCircle(x: x, y: y, d: d){
+//            print("실패 좌표 (\(x),\(y))이고 아웃 카운트는 \(outPoint)")
+            x -= k
+            guard x >= 0 else {x += k; return result}
+            outPoint += 1
         }
         
-        //2) 방어권이 없다면 (-) 되기 전에 종료
-        if defense == 0 {
-            for eachRound in cropEnemy {
-                guard user != 0 else {return clearRound}
-                guard user > 0 else {return clearRound - 1}
-                user -= eachRound
-                clearRound += 1
-            }
-            return clearRound
-        }
-        
-            //3) 방어권이 있다면
-            //3-1) cropEnemy의 합이 음수가 되는 index 불러오기
-            let minusSumIndex = searchArangeIdex(user, cropEnemy)
-            let plusSumIndex = minusSumIndex - 1
-            //3-2) 사용 후 user 값 계산하기
-            var testEnemy = cropEnemy[0...minusSumIndex]
-//            print("프린트 마이너스 인덱스 \(minusSumIndex)")
-            let maxRound = testEnemy.max()! //삭제할 최대 값
-            user = user - testEnemy.reduce(0,{$0+$1}) + maxRound
-//            print("\(user) = user - \(testEnemy.reduce(0,{$0+$1})) + \(maxRound)")
-            //3-3) 지난 라운드 만큼 배열 줄이기
-            cropEnemy.removeSubrange(0...minusSumIndex)
-            clearRound += minusSumIndex + 1
-            defense -= 1
-            
+//        print("성공 좌표 (\(x),\(y))이고 아웃 카운트는 \(outPoint)")
+        result += Int64(fullLineCount - outPoint)
     }
-    
-    return clearRound
-}
- 
 
-// MARK: - 디펜스 안해도 되는 지
-func checkClear (_ n:Int, _ enemy:[Int]) -> Bool {
-    if n >= enemy.reduce(0, {$0+$1}) {
-        return true
-    }
-    return false
-}
-
- 
-// MARK: - 디펜스 해야할 때
-//우리 병사 수가 양수가 아니게 되는 최대 index
-func searchArangeIdex (_ n:Int, _ enemy:[Int]) -> Int {
-    var maximumIdex = -1
-    var totalEnemyCount = 0
-    for eachEnemy in enemy {
-        guard totalEnemyCount < n else {return maximumIdex}
-        totalEnemyCount += eachEnemy
-//        print("enemy, totalEnemyCount:\(enemy),\(totalEnemyCount)")
-        maximumIdex += 1
-    }
-    return maximumIdex
+    return result
 }
 
 
-print(solution(2,4,[3,3,3,3]))//4
-print("다음문제🔥")
-print(solution(7,3,[4,2,4,5,3,3,1]))//5
-print("다음문제🔥")
-print(solution(1,3,[1,1,1,1,3,3,1]))//4
-print("다음문제🔥")
-print(solution(4,1,[4,4,4,5,3,3,1]))//2
-print("다음문제🔥")
-print(solution(5,2,[1,2,1,3,3,3,1]))//5
-print("다음문제🔥")
-print(solution(5,2,[1,2,1,3,3,3,1]))//5
-print("다음문제🔥")
-print(solution(4,1,[4,2,2]))//3
+func checkInnerCircle(x:Int, y:Int, d:Int) -> Bool {
+    var check:Bool = false
+    if x*x + y*y <= d*d {
+        check = true
+    }
+    return check
+}
+
+
+
+print(solution(2, 4))//6
+print(solution(1, 5))//26
 
