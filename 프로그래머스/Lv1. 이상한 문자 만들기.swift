b3cd47ea2e479aa1da85acf91b197b37ec9ac3fc
hyeunPark <shippingbok@gmail.com>
2022-12-28 10:21:03 +0900

    22.12.28

--- 프로그래머스/Lv1. 이상한 문자 만들기.swift
+++ 프로그래머스/Lv1. 이상한 문자 만들기.swift
@@ -4,9 +4,11 @@
 
 import Foundation
 
+// MARK: - joined()도 reduce도 가능
+
 func solution(_ s:String) -> String {
     let sArray = s.map{$0}
-    return (1...s.count).map{$0 % 2 == 1 ? sArray[$0-1].uppercased() : sArray[$0-1].lowercased()}.reduce(""){$0 + $1}
+    return (1...s.count).map{$0 % 2 == 1 ? sArray[$0-1].uppercased() : sArray[$0-1].lowercased()}.joined()
 }
 
 print(solution("try hello world"))
