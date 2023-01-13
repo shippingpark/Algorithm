0d56ecd66afa739a4e77776f86a22db87eed862d
hyeunPark <shippingbok@gmail.com>
2023-01-13 13:17:46 +0900

    23.01.13
    <23.01.13 모닝 알고리즘>
    [출처: 프로그래머스]
    (참가: 찌/무/든)
    
    (1) Lv1. 크기가 작은 부분 문자열

---
+++ 프로그래머스/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Algorithm
+//
+//  Created by 박혜운 on 2023/01/13.
+//
+
+import Foundation
--- 프로그래머스/main.swift
+++ 프로그래머스/main.swift
@@ -7,3 +7,24 @@
 
 import Foundation
 
+func solution(_ t:String, _ p:String) -> Int {
+    let pLength = p.count
+    let tLength = t.count
+    var tArray:[String] = []
+    
+    (0...tLength-pLength).forEach{
+        tArray.append(t.map{String($0)}[$0...$0+pLength-1].joined())
+    }
+    print(tArray)
+    return tArray.filter{Int($0)! <= Int(p)!}.count
+}
+
+
+
+//일단!!! pString을
+print(solution("3141592", "271")) //2
+print(solution("500220839878", "7")) //8
+
+print(solution("10203", "15")) //3
+
+
