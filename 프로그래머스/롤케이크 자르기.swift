9e66ed64f31d4e55f5ea5ab1ea078aa1d315818a
hyeun <shippingbok@gmail.com>
2023-04-15 14:03:28 +0900

    23.04.15
    롤케이크 자르기

---
+++ 프로그래머스/롤케이크 자르기.swift
@@ -0,0 +1,37 @@
+//
+//  롤케이크 자르기.swift
+//  Algorithm
+//
+//  Created by 박혜운 on 2023/04/15.
+//
+
+import Foundation
+
+func solution(_ topping:[Int]) -> Int {
+    var leftCakeSet = Set<Int>()
+    var rightCakeSet = Set<Int>()
+    var leftCake:[Int] = []
+    var rightCake:[Int] = []
+    var result = 0
+    
+    
+    for leftTopping in topping {
+        leftCakeSet.insert(leftTopping)
+        leftCake.append(leftCakeSet.count)
+    }
+    
+    for rightTopping in topping.reversed() {
+        rightCakeSet.insert(rightTopping)
+        rightCake.append(rightCakeSet.count)
+    }
+    
+    rightCake.reverse()
+    
+    for i in 0..<(topping.count-1) {
+        if leftCake[i] == rightCake[i+1] {
+            result += 1
+        }
+    }
+    
+    return result
+}
