2adda8e726724854489b232767343c7fbc30abf6
hyeun <shippingbok@gmail.com>
2023-05-08 20:50:46 +0900

    23.05.08
    봄버맨

--- 백준/DFS와 BFS.swift
+++ 백준/DFS와 BFS.swift
@@ -7,55 +7,61 @@
 
 import Foundation
 
-let input = readLine()!.components(separatedBy: " ").map{Int($0)!}
-let N = input[0]
-let M = input[1]
-let V = input[2]
+// MARK: - 내 풀이
 
-var graph:[[Int]] = {
-    var graph:[[Int]] = Array(repeating: [], count: N+1)
-    for _ in 0..<M {
-        let relation = readLine()!.split(separator: " ").map{Int(String($0))!}
-        graph[relation[0]].append(relation[1])
-        graph[relation[1]].append(relation[0])
-        graph[relation[0]].sort()
-        graph[relation[1]].sort()
-    }
-    return graph
-}()
 
-var check = Array(repeating: false, count: N+1)
-var resultDfs = ""
+//let input = readLine()!.components(separatedBy: " ").map{Int($0)!}
+//let N = input[0]
+//let M = input[1]
+//let V = input[2]
+//
+//var graph:[[Int]] = {
+//    var graph:[[Int]] = Array(repeating: [], count: N+1)
+//    for _ in 0..<M {
+//        let relation = readLine()!.split(separator: " ").map{Int(String($0))!}
+//        graph[relation[0]].append(relation[1])
+//        graph[relation[1]].append(relation[0])
+//        graph[relation[0]].sort()
+//        graph[relation[1]].sort()
+//    }
+//    return graph
+//}()
+//
+//var check = Array(repeating: false, count: N+1)
+//var resultDfs = ""
+//
+//func dfsSolve(_ now: Int) {
+//    check[now] = true
+//    resultDfs += "\(now) "
+//    for i in graph[now] {
+//        if !check[i] {
+//            dfsSolve(i)
+//        }
+//    }
+//}
+//
+//func bfsSolve(_ now: Int) {
+//    var visited:Set<Int> = [now]
+//    var queue:[Int] = graph[now]
+//    var result = "\(now) "
+//
+//    while !queue.isEmpty {
+//        let node = queue.removeFirst()
+//        if !visited.contains(node) {
+//            visited.insert(node)
+//            result += "\(node) "
+//            queue.append(contentsOf: graph[node])
+//        }
+//    }
+//    print(result)
+//}
 
-func dfsSolve(_ now: Int) {
-    check[now] = true
-    resultDfs += "\(now) "
-    for i in graph[now] {
-        if !check[i] {
-            dfsSolve(i)
-        }
-    }
-}
+//dfsSolve(V)
+//print(resultDfs)
+//bfsSolve(V)
 
-func bfsSolve(_ now: Int) {
-    var visited:Set<Int> = [now]
-    var queue:[Int] = graph[now]
-    var result = "\(now) "
+// MARK: - 타인 풀이 검증
 
-    while !queue.isEmpty {
-        let node = queue.removeFirst()
-        if !visited.contains(node) {
-            visited.insert(node)
-            result += "\(node) "
-            queue.append(contentsOf: graph[node])
-        }
-    }
-    print(result)
-}
-
-dfsSolve(V)
-print(resultDfs)
-bfsSolve(V)
 
 //var checklist = Array(repeating: true, count: N+1)
 //var queue: [Int] = []
