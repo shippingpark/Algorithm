958541c454890ef213c7c144f040e14b5acf9df6
hyeun <shippingbok@gmail.com>
2023-05-22 13:08:22 +0900

    23.05.22

--- 백준/DFS와 BFS.swift
+++ 백준/DFS와 BFS.swift
@@ -10,55 +10,55 @@ import Foundation
 // MARK: - 내 풀이
 
 
-//let input = readLine()!.components(separatedBy: " ").map{Int($0)!}
-//let N = input[0]
-//let M = input[1]
-//let V = input[2]
-//
-//var graph:[[Int]] = {
-//    var graph:[[Int]] = Array(repeating: [], count: N+1)
-//    for _ in 0..<M {
-//        let relation = readLine()!.split(separator: " ").map{Int(String($0))!}
-//        graph[relation[0]].append(relation[1])
-//        graph[relation[1]].append(relation[0])
-//        graph[relation[0]].sort()
-//        graph[relation[1]].sort()
-//    }
-//    return graph
-//}()
-//
-//var check = Array(repeating: false, count: N+1)
-//var resultDfs = ""
-//
-//func dfsSolve(_ now: Int) {
-//    check[now] = true
-//    resultDfs += "\(now) "
-//    for i in graph[now] {
-//        if !check[i] {
-//            dfsSolve(i)
-//        }
-//    }
-//}
-//
-//func bfsSolve(_ now: Int) {
-//    var visited:Set<Int> = [now]
-//    var queue:[Int] = graph[now]
-//    var result = "\(now) "
-//
-//    while !queue.isEmpty {
-//        let node = queue.removeFirst()
-//        if !visited.contains(node) {
-//            visited.insert(node)
-//            result += "\(node) "
-//            queue.append(contentsOf: graph[node])
-//        }
-//    }
-//    print(result)
-//}
+let input = readLine()!.components(separatedBy: " ").map{Int($0)!}
+let N = input[0]
+let M = input[1]
+let V = input[2]
 
-//dfsSolve(V)
-//print(resultDfs)
-//bfsSolve(V)
+var graph:[[Int]] = {
+    var graph:[[Int]] = Array(repeating: [], count: N+1)
+    for _ in 0..<M {
+        let relation = readLine()!.split(separator: " ").map{Int(String($0))!}
+        graph[relation[0]].append(relation[1])
+        graph[relation[1]].append(relation[0])
+        graph[relation[0]].sort()
+        graph[relation[1]].sort()
+    }
+    return graph
+}()
+
+var check = Array(repeating: false, count: N+1)
+var resultDfs = ""
+
+func dfsSolve(_ now: Int) {
+    check[now] = true
+    resultDfs += "\(now) "
+    for i in graph[now] {
+        if !check[i] {
+            dfsSolve(i)
+        }
+    }
+}
+
+func bfsSolve(_ now: Int) {
+    var visited:Set<Int> = [now]
+    var queue:[Int] = graph[now]
+    var result = "\(now) "
+
+    while !queue.isEmpty {
+        let node = queue.removeFirst()
+        if !visited.contains(node) {
+            visited.insert(node)
+            result += "\(node) "
+            queue.append(contentsOf: graph[node])
+        }
+    }
+    print(result)
+}
+
+dfsSolve(V)
+print(resultDfs)
+bfsSolve(V)
 
 // MARK: - 타인 풀이 검증
 
---
+++ 프로그래머스/LV2. 큰 수 만들기.swift
@@ -0,0 +1,8 @@
+//
+//  LV2. 큰 수 만들기.swift
+//  Algorithm
+//
+//  Created by 박혜운 on 2023/05/22.
+//
+
+import Foundation
