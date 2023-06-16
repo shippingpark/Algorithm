41868ca73f761fea54a97a39cd87a25dc9ec8447
hyeun <81788774+shippingpark@users.noreply.github.com>
2023-06-16 23:12:35 +0900

    23.06.16

---
+++ 프로그래머스/Lv2. 의상.swift
@@ -0,0 +1,18 @@
+//
+//  Lv2. 의상.swift
+//  Algorithm
+//
+//  Created by 박혜운 on 2023/06/16.
+//
+
+func solution(_ clothes:[[String]]) -> Int {
+    var types: [String] = clothes.map { $0.last! }
+    var typeSet: Set<String> = Set( types )
+    var category: [String] = Array(typeSet)
+    
+    let count: [Int] = category.map{ type in
+        return (clothes.filter{ $0.last! == type }.count + 1)
+    }
+    
+    return count.reduce(1) { $0 * $1 } - 1
+}
