db962849ccee7963038f95c25cd8390f3f1b416f
bono <shippingbok@gmail.com>
2025-01-13 11:05:56 +0900

    [25.01.13] LinkedList Challenge2 보강

--- LinkedList.playground/Sources/Challenge/FindTheMiddleNode.swift
+++ LinkedList.playground/Sources/Challenge/FindTheMiddleNode.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// MARK: 두 개의 참조 활용
+// MARK: 두 개의 참조 활용, 한 칸씩 이동하는 참조와 두 칸씩 이동하는 참조 : Runner 기법 
 
 /// LinkedList라는 게 head에서부터 시작할 때 모든 값을 훑을 수 있음을 유념해야 할 듯
 /// 두 개의 참조를 활용하라 할떄, 하나는 두 배씩 증가하고 하나는 느리게 증가하는 값을 쓰라는 풀이를 봤을 때
@@ -28,4 +28,8 @@ func getMiddle<T>(_ list: LinkedList<T>) -> Node<T>? {
   return slow
 }
 
+/// 답을 보지 않고 생각했던 Solution은 find(at: list.count/2) 였다
+/// 이런 find 함수는 i 만큼, 정확히는 n/2의 시간이 걸린다
+/// 위의 solution도 n/2만큼의 시간이 걸린다. 왜냐하면 두 칸씩 이동하기 떄문이다 (주의, 두 배씩이 아님)
+/// 즉 어떤 방식이건 O(n/2)의 시간복잡도가 걸린다
 
