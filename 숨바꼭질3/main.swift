705e7d745e9c1b9c37526188e17a0b6da518a745
bono <shippingbok@gmail.com>
2025-03-01 16:06:59 +0900

    [25.03.01] 숨바꼭질3

--- 숨바꼭질3/main.swift
+++ 숨바꼭질3/main.swift
@@ -14,150 +14,160 @@ import Foundation
 // (1) N이 K보다 큰 상황
 // (2) 시간초과
 
+//let input = readLine()!.components(separatedBy: " ").compactMap { Int($0) }
+//let (N, K) = (input[0], input[1])
+//
+//func bfs() {
+//  var visited = Array(repeating: false, count: 100001)
+//  visited[N] = true
+//  var queue = [(N, 0)]
+//  
+//  
+//  while !queue.isEmpty {
+//    let (current, weight) = queue.removeFirst()
+//    
+//    if current == K {
+//      print(weight)
+//      return
+//    }
+//    
+//    if current*2 < 100001 && !visited[current*2] {
+//      visited[current*2] = true
+//      queue.append((current*2, weight))
+//    }
+//    
+//    if current-1 >= 0 && !visited[current-1] {
+//      visited[current-1] = true
+//      queue.append((current-1, weight+1))
+//    }
+//    
+//    if current+1 < 100001 && !visited[current+1] {
+//      visited[current+1] = true
+//      queue.append((current+1, weight+1))
+//    }
+//  }
+//}
+//
+//bfs()
+
+import Foundation
+
 let input = readLine()!.components(separatedBy: " ").compactMap { Int($0) }
 let (N, K) = (input[0], input[1])
 
-func bfs() {
-  var visited = Array(repeating: false, count: 100001)
-  visited[N] = true
-  var queue = [(N, 0)]
+struct Heap<T> {
+  var array = [T]()
+  var sort: (T, T) -> Bool
+  
+  init(array: [T], sort: @escaping (T, T) -> Bool) {
+    self.array = array
+    self.sort = sort
+    
+    for i in stride(from: count/2-1, through: 0, by: -1) {
+      siftDown(at: i)
+    }
+  }
+  
+  var count: Int { return array.count }
+  var isEmpty: Bool { return array.isEmpty }
+  
+  func leftChildIndex(from index: Int) -> Int {
+    return 2 * index + 1
+  }
+  
+  func rightChildIndex(from index: Int) -> Int {
+    return 2 * index + 2
+  }
+  
+  func parentIndex(from index: Int) -> Int {
+    return (index-1) / 2
+  }
+  
+  mutating func append(_ value: T) {
+    array.append(value)
+    siftUp(at: count-1)
+  }
+  
+  mutating func remove() -> T {
+    array.swapAt(0, count-1)
+    defer {
+      siftDown(at: 0)
+    }
+    return array.removeLast()
+  }
+  
+  mutating func siftDown(at index: Int) {
+    var parent = index
+    
+    while true {
+      let left = leftChildIndex(from: index)
+      let right = rightChildIndex(from: index)
+      
+      var candidate = index
+      if count > left && sort(array[left], array[candidate]) {
+        candidate = left
+      }
+      
+      if count > right && sort(array[right], array[candidate]) {
+        candidate = right
+      }
+      
+      if candidate == index { return }
+      
+      array.swapAt(parent, candidate)
+      parent = candidate
+    }
+  }
   
   
-  while !queue.isEmpty {
-    let (current, weight) = queue.removeFirst()
+  mutating func siftUp(at index: Int) {
+    var child = index
+    var parent = parentIndex(from: child)
     
-    if current == K {
-      print(weight)
-      return
-    }
-    
-    if current*2 < 100001 && !visited[current*2] {
-      visited[current*2] = true
-      queue.append((current*2, weight))
-    }
-    
-    if current-1 >= 0 && !visited[current-1] {
-      visited[current-1] = true
-      queue.append((current-1, weight+1))
-    }
-    
-    if current+1 < 100001 && !visited[current+1] {
-      visited[current+1] = true
-      queue.append((current+1, weight+1))
+    while child > 0 && sort(array[child], array[parent]) {
+      array.swapAt(parent, child)
+      child = parent
+      parent = parentIndex(from: child)
     }
   }
 }
 
-bfs()
+func solution_숨바꼭질() {
+  guard N < K else { print(N-K); return }
+  var dist = Array(repeating: Int.max, count: 100_001) // 목적지의 두 배까지 도달할 일은 없으므로
+  dist[N] = 0
+  var pq = Heap(array: [(N, 0)], sort: { $0.1 < $1.1 })
+  
+  while !pq.isEmpty {
+    let (current, currentWeight) = pq.remove()
+    
+    if dist[current] < currentWeight {
+      continue
+    }
+    
+    if current == K {
+      print(currentWeight)
+      return
+    }
+    
+    for i in [current, -1, 1] {
+      let next = current + i
+      
+      guard 0..<100_001 ~= next else { continue }
+      
+      if i == current {
+        if dist[next] > currentWeight {
+          dist[next] = currentWeight
+          pq.append((next, currentWeight))
+        }
+      } else {
+        if dist[next] > currentWeight + 1 {
+          dist[next] = currentWeight + 1
+          pq.append((next, currentWeight + 1))
+        }
+      }
+    }
+  }
+}
 
-//struct Heap<T> {
-//  var array = [T]()
-//  var sort: (T, T) -> Bool
-//  
-//  init(array: [T], sort: @escaping (T, T) -> Bool) {
-//    self.array = array
-//    self.sort = sort
-//    
-//    for i in stride(from: count/2-1, through: 0, by: -1) {
-//      siftDown(at: i)
-//    }
-//  }
-//  
-//  var count: Int { return array.count }
-//  var isEmpty: Bool { return array.isEmpty }
-//  
-//  func leftChildIndex(from index: Int) -> Int {
-//    return 2 * index + 1
-//  }
-//  
-//  func rightChildIndex(from index: Int) -> Int {
-//    return 2 * index + 2
-//  }
-//  
-//  func parentIndex(from index: Int) -> Int {
-//    return (index-1) / 2
-//  }
-//  
-//  mutating func append(_ value: T) {
-//    array.append(value)
-//    siftUp(at: count-1)
-//  }
-//  
-//  mutating func remove() -> T {
-//    array.swapAt(0, count-1)
-//    defer {
-//      siftDown(at: 0)
-//    }
-//    return array.removeLast()
-//  }
-//  
-//  mutating func siftDown(at index: Int) {
-//    var parent = index
-//    
-//    while true {
-//      let left = leftChildIndex(from: index)
-//      let right = rightChildIndex(from: index)
-//      
-//      var candidate = index
-//      if count > left && sort(array[left], array[candidate]) {
-//        candidate = left
-//      }
-//      
-//      if count > right && sort(array[right], array[candidate]) {
-//        candidate = right
-//      }
-//      
-//      if candidate == index { return }
-//      
-//      array.swapAt(parent, candidate)
-//      parent = candidate
-//    }
-//  }
-//  
-//  
-//  mutating func siftUp(at index: Int) {
-//    var child = index
-//    var parent = parentIndex(from: child)
-//    
-//    while child > 0 && sort(array[child], array[parent]) {
-//      array.swapAt(parent, child)
-//      child = parent
-//      parent = parentIndex(from: child)
-//    }
-//  }
-//}
-//
-//func solution_숨바꼭질() {
-//  guard N < K else { print(N-K); return }
-//  var dist = Array(repeating: 1000000, count: K*2) // 목적지의 두 배까지 도달할 일은 없으므로
-//  dist[N] = 0
-//  var pq = Heap(array: [(N, 0)], sort: { $0.1 < $1.1 })
-//  // [(N, 0)] // (index, weight)
-//  
-//  while !pq.isEmpty {
-////    let index = pq.indices.min { pq[$0].1 < pq[$1].1 }! // 시간이 적은 것 부터
-//    let (current, currentWeight) = pq.remove()
-//    
-//    if current == K {
-//      print(currentWeight)
-//      return
-//    }
-//    
-//    guard dist[current] <= currentWeight else { continue }
-//    
-//    if current > 0 && dist[current-1] > dist[current]+1 {
-//      dist[current-1] = dist[current]+1
-//      pq.append((current-1, currentWeight+1))
-//    }
-//    if current < K*2-1 && dist[current+1] > dist[current]+1 {
-//      dist[current+1] = dist[current]+1
-//      pq.append((current+1, currentWeight+1))
-//    }
-//    if current*2 < K*2-1 && dist[current*2] > dist[current] {
-//      dist[current*2] = dist[current]
-//      pq.append((current*2, currentWeight))
-//    }
-//  }
-//}
-//
-//solution_숨바꼭질()
+solution_숨바꼭질()
